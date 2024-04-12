773a0b7a37390aefc174cf3472877f66dbc9f461
YourBoyRory <yourboyrory@gmail.com>
2024-04-12 18:01:05 -0400

    first and only

--- unhideBatteryUPS.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-chargedIcon="/usr/share/icons/Papirus/16x16/panel/battery-100-charged.svg"
-devicesIcon="/usr/share/icons/Papirus/16x16/panel/system-devices-panel.svg"
-
-sudo cp "$chargedIcon.bak" "$chargedIcon" && sudo rm "$chargedIcon.bak"
